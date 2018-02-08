ec015032a7f0803adc7a5bcd83c94f3f6a1651b8
Stanislav <its.an.omen.man@gmail.com>
2018-02-08 21:15:20 +0200

    Add registration page (adaptive)|Add 2 controllers for demo|Refactoring

--- src/main/java/edu/netcracker/project/logistic/Application.java
+++ src/main/java/edu/netcracker/project/logistic/Application.java
@@ -1,9 +1,9 @@
-package logistics;
+package edu.netcracker.project.logistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "edu.netcracker.project.logistic" })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
