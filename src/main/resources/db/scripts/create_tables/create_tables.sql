46906f5560651a31c1e56e50226b4afc1ac5e02f
Bogdan <bogdan.zsnkevich@ukr.net>
2018-02-09 17:27:45 +0200

    Scripts for registration

--- src/main/resources/db/scripts/create_tables/create_tables.sql
+++ src/main/resources/db/scripts/create_tables/create_tables.sql
@@ -59,7 +59,7 @@ ALTER  TABLE  "logistic_company"."employee_role" ADD  FOREIGN KEY  ("role_id") R
 ALTER  TABLE  "logistic_company"."employee_role" ADD  FOREIGN KEY ("employee_id") REFERENCES  "logistic_company"."employee"("employee_id");
 
 
-
-INSERT INTO "logistic_company"."person"(first_name, last_name, nick_name, password, registration_date, email, phone_number) VALUES ('Bogdan', 'Zinkevich', 'Zibo',121212,'1997-10-11','bogdan@gamil.com','+380687072934');
-INSERT INTO  "logistic_company"."role"(role_name) VALUES ('ADMIN');
-INSERT INTO  "logistic_company"."user"(user_id, "role_id ", permission_id) VALUES ('3','4','1');
+--
+-- INSERT INTO "logistic_company"."person"(first_name, last_name, nick_name, password, registration_date, email, phone_number) VALUES ('Bogdan', 'Zinkevich', 'Zibo',121212,'1997-10-11','bogdan@gamil.com','+380687072934');
+-- INSERT INTO  "logistic_company"."role"(role_name) VALUES ('ADMIN');
+-- INSERT INTO  "logistic_company"."user"(user_id, "role_id ", permission_id) VALUES ('3','4','1');
