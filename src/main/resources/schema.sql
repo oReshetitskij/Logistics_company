cdf7438ed85687cbb313bd2bcc1b9bd4a45ffbc9
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2018-02-09 22:27:06 +0200

    Add Spring JDBC Add connection parameters Add registration data table Format sql scripts

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,66 +1,97 @@
-CREATE SEQUENCE "logistic_company"."main_seq_id"
- INCREMENT 1
- MINVALUE 1
- MAXVALUE 9223372036854775807
- START 1
- CACHE 10;
-SELECT setval('"logistic_company"."main_seq_id"', 1, true);
+DROP TABLE IF EXISTS "logistic_company"."employee_role";
+DROP TABLE IF EXISTS "logistic_company"."user";
+DROP TABLE IF EXISTS "logistic_company"."employee";
+DROP TABLE IF EXISTS "logistic_company"."person";
+DROP TABLE IF EXISTS "logistic_company"."role";
+DROP TABLE IF EXISTS "logistic_company"."registration_data";
+DROP SEQUENCE IF EXISTS "logistic_company"."main_seq_id";
+DROP SCHEMA IF EXISTS "logistic_company";
 
+CREATE SCHEMA "logistic_company";
+
+CREATE SEQUENCE "logistic_company"."main_seq_id"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 10;
+
+SELECT setval('"logistic_company"."main_seq_id"', 1, TRUE);
+
+SET SEARCH_PATH TO logistic_company;
 
 CREATE TABLE "logistic_company"."person" (
-"person_id" int4 DEFAULT nextval('main_seq_id'::regclass) NOT NULL,
-"first_name" varchar(45) COLLATE "default" NOT NULL,
-"last_name" varchar(45) COLLATE "default" NOT NULL,
-"nick_name" varchar(45) COLLATE "default",
-"password" varchar(200) COLLATE "default" NOT NULL,
-"registration_date" date ,
-"email" varchar(45) COLLATE "default" NOT NULL,
-"phone_number" varchar(45) COLLATE "default"
+  "person_id"         INT4 DEFAULT nextval('main_seq_id' :: REGCLASS) NOT NULL,
+  "first_name"        VARCHAR(45) COLLATE "default"                   NOT NULL,
+  "last_name"         VARCHAR(45) COLLATE "default"                   NOT NULL,
+  "nick_name"         VARCHAR(45) COLLATE "default",
+  "password"          VARCHAR(200) COLLATE "default"                  NOT NULL,
+  "registration_date" DATE,
+  "email"             VARCHAR(45) COLLATE "default"                   NOT NULL,
+  "phone_number"      VARCHAR(45) COLLATE "default"
 )
-WITH (OIDS=FALSE);
+WITH (OIDS = FALSE
+);
 
 
 CREATE TABLE "logistic_company"."user" (
-"user_id" int4,
-"role_id " int4,
-"permission_id" int4
+  "user_id"       INT4,
+  "role_id"       INT4,
+  "permission_id" INT4
 )
-WITH (OIDS=FALSE);
+WITH (OIDS = FALSE
+);
 
 CREATE TABLE "logistic_company"."employee" (
-"employee_id" int4,
-"salary" numeric,
-"manager_id" int4,
-"work_days_id" int4
+  "employee_id"  INT4,
+  "salary"       NUMERIC,
+  "manager_id"   INT4,
+  "work_days_id" INT4
 )
-WITH (OIDS=FALSE);
-
-CREATE TABLE "logistic_company"."role"(
-"role_id" int4 DEFAULT nextval('main_seq_id'::regclass) NOT NULL,
-"role_name" varchar(45) COLLATE "default" NOT NULL
+WITH (OIDS = FALSE
 );
 
-CREATE  TABLE  "logistic_company"."employee_role"
+CREATE TABLE "logistic_company"."role" (
+  "role_id"   INT4 DEFAULT nextval('main_seq_id' :: REGCLASS) NOT NULL,
+  "role_name" VARCHAR(45) COLLATE "default"                   NOT NULL
+);
+
+CREATE TABLE "logistic_company"."employee_role"
 (
- "employee_id" int4,
- "role_id" int4
+  "employee_id" INT4,
+  "role_id"     INT4
+);
+
+CREATE TABLE "logistic_company"."registration_data"
+(
+  "registration_data_id" INT4 DEFAULT nextval('main_seq_id') NOT NULL,
+  "first_name"           VARCHAR(45)                         NOT NULL,
+  "last_name"            VARCHAR(45)                         NOT NULL,
+  "nick_name"            VARCHAR(45),
+  "password"             VARCHAR(200)                        NOT NULL,
+  "email"                VARCHAR(45)                         NOT NULL,
+  "phone_number"         VARCHAR(45)
 );
 
 
-ALTER  TABLE  "logistic_company"."person" ADD  PRIMARY KEY ("person_id");
-ALTER  TABLE  "logistic_company"."employee" ADD PRIMARY KEY ("employee_id");
-ALTER  TABLE "logistic_company"."user" ADD PRIMARY KEY ("user_id");
-ALTER TABLE "logistic_company"."role" ADD PRIMARY KEY ("role_id");
+ALTER TABLE "logistic_company"."person"
+  ADD PRIMARY KEY ("person_id");
+ALTER TABLE "logistic_company"."employee"
+  ADD PRIMARY KEY ("employee_id");
+ALTER TABLE "logistic_company"."user"
+  ADD PRIMARY KEY ("user_id");
+ALTER TABLE "logistic_company"."role"
+  ADD PRIMARY KEY ("role_id");
+ALTER TABLE "logistic_company"."registration_data"
+  ADD PRIMARY KEY ("registration_data_id");
 
-ALTER TABLE "logistic_company"."user" ADD FOREIGN KEY("user_id") REFERENCES  "logistic_company"."person"("person_id");
-ALTER  TABLE "logistic_company"."employee" ADD  FOREIGN KEY ("employee_id") REFERENCES  "logistic_company"."person"("person_id");
-ALTER TABLE "logistic_company"."user" ADD  FOREIGN KEY ("role_id ") REFERENCES "logistic_company"."role"("role_id");
-ALTER  TABLE  "logistic_company"."employee_role" ADD  FOREIGN KEY  ("role_id") REFERENCES "logistic_company"."role"("role_id");
-ALTER  TABLE  "logistic_company"."employee_role" ADD  FOREIGN KEY ("employee_id") REFERENCES  "logistic_company"."employee"("employee_id");
-
-DROP TABLE IF EXISTS "logistic_company"."employee_role";
-DROP TABLE if EXISTS "logistic_company"."user";
-DROP TABLE if EXISTS "logistic_company"."employee";
-DROP  TABLE IF EXISTS "logistic_company"."person";
-DROP TABLE IF EXISTS "logistic_company"."role";
-
+ALTER TABLE "logistic_company"."user"
+  ADD FOREIGN KEY ("user_id") REFERENCES "logistic_company"."person" ("person_id");
+ALTER TABLE "logistic_company"."employee"
+  ADD FOREIGN KEY ("employee_id") REFERENCES "logistic_company"."person" ("person_id");
+ALTER TABLE "logistic_company"."user"
+  ADD FOREIGN KEY ("role_id") REFERENCES "logistic_company"."role" ("role_id");
+ALTER TABLE "logistic_company"."employee_role"
+  ADD FOREIGN KEY ("role_id") REFERENCES "logistic_company"."role" ("role_id");
+ALTER TABLE "logistic_company"."employee_role"
+  ADD FOREIGN KEY ("employee_id") REFERENCES "logistic_company"."employee" ("employee_id");
