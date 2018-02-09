4b87702534025cea93949966c8ef6aeea3c4d6cb
Stanislav <its.an.omen.man@gmail.com>
2018-02-09 15:19:05 +0200

    Dao for user

---
+++ src/main/java/edu/netcracker/project/logistic/Dao/Impl/UserCrudDaoImpl.java
@@ -0,0 +1,33 @@
+package edu.netcracker.project.logistic.Dao.Impl;
+
+import edu.netcracker.project.logistic.Dao.UserCrudDao;
+import edu.netcracker.project.logistic.model.User;
+
+public class UserCrudDaoImpl implements UserCrudDao {
+
+
+    @Override
+    public User save(User object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(User object) {
+
+    }
+
+    @Override
+    public User find_one(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Long aLong) {
+        return false;
+    }
+}
