95413e7d27a05ad96e90c75305bcc3f6830e9a50
Stanislav <its.an.omen.man@gmail.com>
2018-02-10 17:44:44 +0200

    Security configuration | Log in/out for user and admin (in memory)

---
+++ src/main/java/edu/netcracker/project/logistic/config/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,72 @@
+package edu.netcracker.project.logistic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collection;
+
+@Configuration
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+
+
+
+    private void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()){
+            // debug
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    private String determineTargetUrl(Authentication authentication) {
+        boolean isUser = false;
+        boolean isAdmin = false;
+        Collection<? extends GrantedAuthority> authorities =
+                authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities){
+            if (grantedAuthority.getAuthority().equals("ROLE_USER")){
+                isUser = true;
+                break;
+            } else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")){
+                isAdmin = true;
+                break;
+            }
+        }
+
+        if (isUser) {
+            return "user";
+        } else if (isAdmin) {
+            return "admin";
+        } else
+            throw new IllegalStateException();
+        }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null){
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+
+}
--- src/main/java/edu/netcracker/project/logistic/controllers/TestController.java
+++ src/main/java/edu/netcracker/project/logistic/controllers/TestController.java
@@ -7,6 +7,7 @@ import edu.netcracker.project.logistic.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Date;
@@ -34,4 +35,22 @@ public class TestController {
 
         return "test";
     }
+
+
+
+    @GetMapping("/error/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
 }
