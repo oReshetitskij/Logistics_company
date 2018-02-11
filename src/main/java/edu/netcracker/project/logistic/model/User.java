ec16605cdca74966e49034ddecffbd7ead1850aa
Bogdan <bogdan.zsnkevich@ukr.net>
2018-02-12 01:28:40 +0200

    create model Role

--- src/main/java/edu/netcracker/project/logistic/model/User.java
+++ src/main/java/edu/netcracker/project/logistic/model/User.java
@@ -1,34 +1,37 @@
 package edu.netcracker.project.logistic.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User extends Person {
-    private List<Long> role_id;
-    private List<Long> permission_id;
+    private List<Role> roles = new ArrayList<>();
+    private List<Permission> permissions = new ArrayList<>();
 
-
-    public User(Long id, String firstName, String lastName, String nickName, String password, LocalDate registrationDate, String email, String phoneNumber, List<Long> role_id, List<Long> permission_id) {
+    public User(Long id, String firstName, String lastName, String nickName, String password, LocalDate registrationDate, String email, String phoneNumber, List<Role> roles, List<Permission> permissions) {
         super(id, firstName, lastName, nickName, password, registrationDate, email, phoneNumber);
-        this.role_id = role_id;
-        this.permission_id = permission_id;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+    public User()
+    {
+
+
     }
 
-    public List<Long> getRole_id() {
-        return role_id;
+    public List<Role> getRoles() {
+        return roles;
     }
 
-    public void setRole_id(List<Long> role_id) {
-        this.role_id = role_id;
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
-    public List<Long> getPermission_id() {
-        return permission_id;
+    public List<Permission> getPermissions() {
+        return permissions;
     }
 
-    public void setPermission_id(List<Long> permission_id) {
-        this.permission_id = permission_id;
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
     }
-
-
 }
