f08a0a20f9b83e116e46bf4b12f475bceb9ad0fa
Bogdan <bogdan.zsnkevich@ukr.net>
2018-02-11 23:45:44 +0200

    update package

--- src/main/java/edu/netcracker/project/logistic/config/WebMvcConfig.java
+++ src/main/java/edu/netcracker/project/logistic/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package edu.netcracker.project.logistic.controllers.loginConfiguration;
+package edu.netcracker.project.logistic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
