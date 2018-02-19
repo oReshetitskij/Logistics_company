056ee5f7d71ec35e9576b7ba4df15295118ed6ec
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2018-02-19 23:03:08 +0200

    Add method for batch delete in PersonRoleDao

--- src/main/java/edu/netcracker/project/logistic/dao/PersonRoleDao.java
+++ src/main/java/edu/netcracker/project/logistic/dao/PersonRoleDao.java
@@ -2,9 +2,11 @@ package edu.netcracker.project.logistic.dao;
 
 import edu.netcracker.project.logistic.model.Person;
 import edu.netcracker.project.logistic.model.PersonRole;
+import edu.netcracker.project.logistic.model.Role;
 
 import java.util.List;
 import java.util.Set;
 
 public interface PersonRoleDao extends CrudDao<PersonRole, PersonRole> {
+    void deleteMany(List<PersonRole> personRoles);
 }
